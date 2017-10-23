51ce9040a08761588a977c0ba5edd5521d391ddc
larlence100 <875394153@qq.com>
2017-10-23 12:32:24 +0800

    commit

--- Application/Api/Controller/CommonController.class.php
+++ Application/Api/Controller/CommonController.class.php
@@ -22,26 +22,30 @@ class CommonController extends ApiController
         $this->returnApiSuccessWithData($fyzidua->select());
     }
 
-    public function getHouseEstate()
+    public function get_xiaoqu()
     {
-        $txt=I('txt');
+        $txt=I('xiaoqum');
         $listObj = M('xiaoqu');
 
         $where['_string']='(xiaoqum like "%'.$txt.'%")  OR (pinyinjs like "%'.$txt.'%")';
-        $where['gongsiid'] = session('gongsiid');
         $list = $listObj->where($where)->limit(10)->select();
         $data=array('status'=>0,'district'=>$list);
-        header("Content-type: application/json");
-        exit(json_encode($data));
+        $this->returnApiSuccessWithData($data);
     }
 
 
+    /**
+     * ajax图片上传
+     */
     public function ajax_upload(){
         // 根据自己的业务调整上传路径、允许的格式、文件大小
         //$path = "/upload/".date('Y').'/'.date('m').'/'.date('d');
         ajaxUpload();
     }
 
+    /**
+     * 获取城市下面的区
+     */
     public function get_area_list()
     {
         $cityId = I('cityid');
