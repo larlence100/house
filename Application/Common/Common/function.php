05bdc8ccb796fdb70db2de694b7ead6f9e1ac9e2
liujianjiang <875394153@qq.com>
2017-10-24 20:45:37 +0800

    commit

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -114,8 +114,8 @@ function define_str_replace($data){
     import('Org.Weixin.errorCode');
     import('Org.Weixin.wxBizDataCrypt');
 
-    $appid= '';
-    $secret= '';
+    $appid= 'wxd60a9da2a894158b';
+    $secret= 'f63615e5126f553e1f35e80e48fb2411';
     $grant_type='authorization_code';
     $url='https://api.weixin.qq.com/sns/jscode2session';
     $url= sprintf("%s?appid=%s&secret=%s&js_code=%s&grant_type=%",$url,$appid,$secret,$code,$grant_type);
