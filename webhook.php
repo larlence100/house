11aa8e585d6b184e1f2fb8f6f897e47344a08c93
larlence100 <875394153@qq.com>
2017-10-31 15:42:43 +0800

    commit

--- webhook.php
+++ webhook.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting ( E_ALL );
-$dir = '/home/house/';//该目录为git检出目录
+$dir = '/home/wwwroot/house/';//该目录为git检出目录
 $handle = popen('cd '.$dir.' && git checkout . && git pull 2>&1','r');
 $read = stream_get_contents($handle);
 printf($read);
