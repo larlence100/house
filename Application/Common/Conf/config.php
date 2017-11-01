fe02f57ebe6fdc1f0eb1b880e758adb09277be95
liujianjiang <875394153@qq.com>
2017-11-01 20:33:19 +0800

    commit

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -18,7 +18,7 @@
 		'DB_HOST' => '118.31.33.47', // 服务器地址
 		'DB_NAME' => 'kaidandashi', // 数据库名
 		'DB_USER' => 'root', // 用户名
-		'DB_PWD' => '123456', // 密码
+		'DB_PWD' => 'root', // 密码
 		'DB_PORT' => '3306', // 端口
 		'DB_PREFIX' => 'jjrxt_', // 数据库表前缀
 		'DB_CHARSET' => 'utf8', // 数据库编码
