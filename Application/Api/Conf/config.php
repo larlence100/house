1234447464aa06de86ba7a71c6431ff09157466a
liujianjiang <875394153@qq.com>
2017-11-19 16:08:19 +0800

    commit

--- Application/Api/Conf/config.php
+++ Application/Api/Conf/config.php
@@ -12,5 +12,7 @@ return array(
 
 	'TWO_DIR' => '',
     'PAY_NOTIFY_URL' => 'http://118.31.33.47/api/pay/notify',
-    'PAY_MONEY' => 0.01
+    'PAY_MONEY' => 0.01,
+    'APPID'=>'wxd60a9da2a894158b',
+    'SECRET'=>'f63615e5126f553e1f35e80e48fb2411'
 );
